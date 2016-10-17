efb729ec3a5426a43e4983e430330ed4b8e2dd59
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-10-17 18:03:33 +0200

    Feauture: retrive markers data from Firebase
    The goal of this new branch is to add a list of Google Maps Places
    in the Firebase Realtime Database. They will be loaded into Lake Map as
    an array.

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,17 @@
 var splashPage = document.getElementById('page-splash');
-/**
- * Writes the user's data to the database.
- */
-// [START basic_write]
+
+ placeName = 'san_carlone';
+
+// Writes the user's data to the database.
 function writeUserData(userId, email) {
   firebase.database().ref('users/' + userId).set({
     email: email
   });
 }
-// [END basic_write]
-
-/**
- * The ID of the currently signed-in User. We keep track of this to detect Auth state change events that are just
- * programmatic token refresh but not a User status change.
- */
+// The ID of the currently signed-in User. We keep track of this to detect Auth state change events that are just
+// programmatic token refresh but not a User status change.
 var currentUID;
-
-/**
- * Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
- */
+// Triggers every time there is a change in the Firebase auth state (i.e. user signed-in or user signed out).
 function onAuthStateChanged(user) {
   // We ignore token refresh events.
   if (user && currentUID === user.uid) {
@@ -29,6 +22,18 @@ function onAuthStateChanged(user) {
     currentUID = user.uid;
     splashPage.style.display = 'none';
     writeUserData(user.uid, user.email);
+    return firebase.database().ref('/places/' + placeName + '/results/0/').once('value').then(function(snapshot) {
+      var formatted_address = snapshot.val().formatted_address;
+      location1 = snapshot.val().geometry.location.lat;
+      location2 = snapshot.val().geometry.location.lat;
+      latLng = new google.maps.LatLng(location1, location2);
+
+      var marker = new google.maps.Marker({
+        position: {lat: 45.924453, lng: 8.556276},
+        title: formatted_address,
+        map:map
+      });
+    });
   } else {
     // Display the splash page where you can sign-in.
     splashPage.style.display = '';
@@ -43,10 +48,8 @@ function SignOut() {
     splashPage.style.display = 'inline';
     // [END signout]
   }
-
 }
 window.addEventListener('load', function() {
   // Listen for auth state changes
   firebase.auth().onAuthStateChanged(onAuthStateChanged);
-
 }, false);
