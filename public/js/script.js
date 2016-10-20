178d64be8ba27decd225fbebecfca51e496ad6c6
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-10-20 10:43:04 +0200

    Feature: implementing Knockout

--- public/js/script.js
+++ public/js/script.js
@@ -4,36 +4,55 @@ var places = [
     { name: "san_carlone", category: "architecture", id: "ChIJhQvajYRxhkcRwvU6dVVjF5M"}
 ];
 
-var infowindow = new google.maps.InfoWindow();
-var service = new google.maps.places.PlacesService(map);
 
-var i;
+var Place = function(data) {
+  this.name = ko.observable(data.name);
+};
 
-for (i = 0; i < places.length; i++) {
-  service.getDetails({
-    placeId: places[i].id
-  }, function(place, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      marker = new google.maps.Marker({
-        map: map,
-        catgr: category,
-        position: place.geometry.location
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        'Category: ' + category + '<br>' +
-        place.formatted_address + '</div>');
-        infowindow.open(map, this);
-      });
-    }
+var ViewModel = function() {
+  var self = this;
+  this.placesList = ko.observableArray([]);
+  places.forEach(function(placeItem) {
+    self.placesList.push( new Place(placeItem) );
   });
-}
+  this.currentPlace = ko.observable( this.placesList()[0] );
+  this.placeSelect = function(place){
+    self.currentPlace(place);
+  };
+};
 
-/*    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-      return function() {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
+ko.applyBindings(new ViewModel());
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.924453, lng: 8.556276},
+    zoom: 11
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+
+  var i;
+
+  for (i = 0; i < places.length; i++) {
+    var thisPlace = places[i];
+    service.getDetails({
+      placeId: places[i].id
+    }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent('<div><strong>' + thisPlace.name + '</strong><br>' +
+          'Category: ' +  thisPlace.category + '<br>' +
+          place.formatted_address + '</div>');
+          infowindow.open(map, this);
+        });
       }
-    })(marker, i));
+    });
   }
-*/
+}
