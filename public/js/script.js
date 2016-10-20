20f3f5b9fa68117d0cef6c0337f59681de818814
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-10-20 18:03:52 +0200

    Test: filter markers by category

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,11 @@ var places = [
     { name: "san_carlone", category: "architecture", id: "ChIJhQvajYRxhkcRwvU6dVVjF5M"}
 ];
 
-
+var catFilter = "";
+var thisCategory = "";
 var Place = function(data) {
   this.name = ko.observable(data.name);
+  this.category = ko.observable(data.category);
 };
 
 var ViewModel = function() {
@@ -18,7 +20,42 @@ var ViewModel = function() {
   this.currentPlace = ko.observable( this.placesList()[0] );
   this.placeSelect = function(place){
     self.currentPlace(place);
+    thisCategory = this.category;
+    myFunction();
   };
+
+  /*function hideMarkers(markers) {
+    for (var i = 0; i < markers.length; i++) {
+      console.log(markers);
+      markers[i].setMap(null);
+    }
+  }*/
+
+/*  function checkCategory(filteredCategory) {
+      return filteredCategory == thisCategory;
+  }*/
+
+  function myFunction() {
+    console.log(places.category);
+      catFilter = places.filter (function (checkCategory) {
+        return checkCategory.category == thisCategory;
+      });
+      console.log(catFilter);
+  }
+
+  /*  function filterMarkers(cat) {
+      console.log(cat);
+      item.addEventListener('click', function(){
+        var thcat = cat;
+        if (marker.catgr == thcat || category.length === 0) {
+          marker.setVisible(false);
+        }
+        // Categories don't match
+        else {
+        }
+      });
+    };*/
+
 };
 
 ko.applyBindings(new ViewModel());
@@ -34,18 +71,21 @@ function initMap() {
   var infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
 
-  var i;
+  var i, marker;
 
   for (i = 0; i < places.length; i++) {
     var thisPlace = places[i];
+    var thisCategory = thisPlace.category;
     service.getDetails({
-      placeId: places[i].id
+      placeId: thisPlace.id
     }, function(place, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         marker = new google.maps.Marker({
           map: map,
-          position: place.geometry.location
+          position: place.geometry.location,
+          catgr: thisCategory
         });
+
         google.maps.event.addListener(marker, 'click', function() {
           infowindow.setContent('<div><strong>' + thisPlace.name + '</strong><br>' +
           'Category: ' +  thisPlace.category + '<br>' +
