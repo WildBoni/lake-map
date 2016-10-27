3d45422be7d05cf8eb51fc266e32a7936bf6fd8d
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-10-27 18:19:26 +0200

    IN PROGRESS: knockout markers optimization

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,50 @@
 var places = [
-    { name: "isola_bella", category: "panorama", id: "ChIJI_225H91hkcRVKb21p-agns", lat: "45.89644639999999", lng: "8.526133099999999", address: "Palazzo Borromeo all'Isola Bella, Isola Bella, 28838 Stresa VB, Italia", show: true},
-    { name: "mottarone", category: "panorama", id: "ChIJ8UByCb4KhkcR0E2nV37mBR0", lat: "45.8833333", lng: "8.449999999999999", address: "Mottarone, 28838 Stresa VB, Italia", show: true},
-    { name: "san_carlone", category: "architecture", id: "ChIJhQvajYRxhkcRwvU6dVVjF5M", lat: "45.7702621", lng: "8.5431992", address: "Statua di San Carlo Borromeo, Piazzale San Carlo, 28041 Arona NO, Italia", show: true}
+    { name: "isola_bella", category: "panorama", id: "ChIJI_225H91hkcRVKb21p-agns", lat: "45.89644639999999", lng: "8.526133099999999", address: "Palazzo Borromeo all'Isola Bella, Isola Bella, 28838 Stresa VB, Italia"},
+    { name: "mottarone", category: "panorama", id: "ChIJ8UByCb4KhkcR0E2nV37mBR0", lat: "45.8833333", lng: "8.449999999999999", address: "Mottarone, 28838 Stresa VB, Italia"},
+    { name: "san_carlone", category: "architecture", id: "ChIJhQvajYRxhkcRwvU6dVVjF5M", lat: "45.7702621", lng: "8.5431992", address: "Statua di San Carlo Borromeo, Piazzale San Carlo, 28041 Arona NO, Italia"}
 ];
 var markers = [];
+var map;
+//var infowindow = new google.maps.InfoWindow();
+
+var ViewModel = function() {
+  var self = this;
+  this.placesList = ko.observableArray([]);
+  this.markersList = ko.observableArray([]);
+  self.catFilter = ko.observable();
+
+  //Event Listener for initializing map
+  //http://stackoverflow.com/questions/36267158/uncaught-referenceerror-google-is-not-defined
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 45.924453, lng: 8.556276},
+      zoom: 11
+    });
+  }
+
+  places.forEach(function(placeItem) {
+    self.placesList.push( new Place(placeItem) );
+  });
+
+  self.filterCats = ko.computed(function () {
+    if (!self.catFilter()) {
+      return self.placesList();
+    } else {
+      return ko.utils.arrayFilter(self.placesList(), function (place) {
+        return place.category() == self.catFilter();
+      });
+    }
+  });
+
+  self.filter = function (category) {
+    self.catFilter(category);
+  };
+
+  // https://developers.google.com/maps/documentation/javascript/examples/event-domListener
+  google.maps.event.addDomListener(window, 'load', function(){
+    self.initMap();
+  });
+};
 
 var Place = function(data) {
   this.name = ko.observable(data.name);
@@ -12,78 +53,43 @@ var Place = function(data) {
   this.lat = ko.observable(data.lat);
   this.lng = ko.observable(data.lng);
   this.address = ko.observable(data.address);
-};
 
-var ViewModel = function() {
-  var self = this;
-  this.placesList = ko.observableArray([]);
+  var numlat = parseFloat(this.lat);
+  var numlng = parseFloat(this.lng);
 
-  self.catFilter = ko.observable();
-
-  places.forEach(function(placeItem) {
-    self.placesList.push( new Place(placeItem) );
-  });
-//http://jsfiddle.net/R8dpQ/
-  self.filterCats = ko.computed(function () {
-    if (!self.catFilter()) {
-      return self.placesList();
-    } else {
-      return ko.utils.arrayFilter(self.placesList(), function (place) {
-        return place.category == self.catFilter();
-      });
-    }
+  var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: numlat, lng: numlng},
+    title: this.name,
+    category: this.category,
+    address: this.address
   });
 
-  self.filter = function (category) {
-    self.catFilter(category);
-  };
+  this.marker = ko.observable(marker);
+
+/*  this.marker.addListener('click', function() {
+    map.panTo(new google.maps.LatLng(numlat, numlng));
+    populateInfoWindow(this, infowindow);
+  });*/
 };
 
 ko.applyBindings(new ViewModel());
 
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.924453, lng: 8.556276},
-    zoom: 11
-  });
-
-  for (var i = 0; i < places.length; i++) {
-    var place = places[i];
-    var lat = parseFloat(place.lat);
-    var lng = parseFloat(place.lng);
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-      map: map,
-      position: {lat: lat, lng: lng},
-      title: place.name,
-      category: place.category,
-      address: place.address
-    });
-    //attachInfoWindow(marker);
-    markers.push(marker);
-    marker.addListener('click', function() {
-      map.panTo(new google.maps.LatLng(lat, lng));
-      populateInfoWindow(this, infowindow);
+/*
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div><strong>' + marker.title + '</strong><br>' +
+      'Category: ' + marker.category  + '<br>' +
+      marker.address + '</div>');
+    infowindow.open(map, marker);
+    // http://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function(){ marker.setAnimation(null); }, 1400);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
     });
   }
-
-  function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('<div><strong>' + marker.title + '</strong><br>' +
-        'Category: ' + marker.category  + '<br>' +
-        marker.address + '</div>');
-      infowindow.open(map, marker);
-      // http://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){ marker.setAnimation(null); }, 1400);
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick', function() {
-        infowindow.marker = null;
-      });
-    }
-  }
-}
+}*/
