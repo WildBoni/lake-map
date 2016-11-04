5e71f70b65de7573fe3d2025a6b21da0bf183d13
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-11-04 17:47:22 +0100

    Update: new places and categories inserted

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,13 @@
 var places = [
     { name: "isola_bella", category: "panorama", id: "4bb5f9ff1344b7139acb9c04", lat: "45.89644639999999", lng: "8.526133099999999", address: "Isola Bella, 28838 Stresa VB, Italia"},
     { name: "mottarone", category: "panorama", id: "4c274f81905a0f47eca56460", lat: "45.8833333", lng: "8.449999999999999", address: "Mottarone, 28838 Stresa VB, Italia"},
-    { name: "san_carlone", category: "architecture", id: "4da166849aa4721e1e61fa19", lat: "45.7702621", lng: "8.5431992", address: "Statua di San Carlo Borromeo, Piazzale San Carlo, 28041 Arona NO, Italia"}
+    { name: "san_carlone", category: "architecture", id: "4da166849aa4721e1e61fa19", lat: "45.7702621", lng: "8.5431992", address: "Statua di San Carlo Borromeo, Piazzale San Carlo, 28041 Arona NO, Italia"},
+    { name: "da_Aldo", category: "restaurant", id: "4d04d10a9d33a1432e5bbd78", lat: "45.763473", lng: "8.5564403", address: "Piazza del Popolo, 26, 28041 Arona NO"},
+    { name: "scurone", category: "bar", id: "55d992b8498e8e67796eaf3e", lat: "46.0627611", lng: "8.6976934", address: "Traversa Scurone, 7, 28822 Cannobio VB"}
 ];
 
-// Google maps basic settings
-var map;
-var infowindow = new google.maps.InfoWindow();
-var bounds = new google.maps.LatLngBounds();
-
-//FourSquare credentials
-var clientId = 'CEE3OEEHN1QL2SS1MWKVTAH5L0MYPWFQFSGI2NZWKWMSEXIL';
-var clientSecret = '4NPFBTCE53TLCTJ5DSGEHHVKYXWTE5X5ZDIHP35SE1N0UABR';
-
 // custom markers http://map-icons.com/
-var path;
-var fillColor;
-var map_icon_label;
-
+// assign custom icon to each category
 var createIcons = function(category) {
   switch(category) {
     case "architecture":
@@ -30,6 +20,16 @@ var createIcons = function(category) {
       fillColor = '#2e8234';
       map_icon_label = '<span class="map-icon map-icon-local-government"></span>';
     break;
+    case "restaurant":
+      path = MAP_PIN;
+      fillColor = '#80822e';
+      map_icon_label = '<span class="map-icon map-icon-restaurant"></span>';
+    break;
+    case "bar":
+      path = MAP_PIN;
+      fillColor = '#80822e';
+      map_icon_label = '<span class="map-icon map-bar"></span>';
+    break;
     default:
       path = ROUTE;
       fillColor = '#602e82';
@@ -38,6 +38,18 @@ var createIcons = function(category) {
   }
 };
 
+
+
+// Google maps basic settings
+var map;
+var infowindow = new google.maps.InfoWindow();
+var bounds = new google.maps.LatLngBounds();
+
+// FourSquare credentials
+var clientId = 'CEE3OEEHN1QL2SS1MWKVTAH5L0MYPWFQFSGI2NZWKWMSEXIL';
+var clientSecret = '4NPFBTCE53TLCTJ5DSGEHHVKYXWTE5X5ZDIHP35SE1N0UABR';
+
+// manage everything with konckout.js
 var ViewModel = function() {
   var venues = 'Waiting for input';
   var self = this;
@@ -47,9 +59,8 @@ var ViewModel = function() {
   self.fsName = ko.observable(venues);
   self.fsRating = ko.observable(venues);
 
-
-  //Event Listener for initializing map
-  //http://stackoverflow.com/questions/36267158/uncaught-referenceerror-google-is-not-defined
+  // Event Listener for initializing map
+  // http://stackoverflow.com/questions/36267158/uncaught-referenceerror-google-is-not-defined
   self.initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 45.924453, lng: 8.556276},
@@ -57,6 +68,7 @@ var ViewModel = function() {
     });
   };
 
+  // Adding a listener on each marker
   self.createMarkersClick = function(){
     self.placesList().forEach(function(place){
       google.maps.event.addListener(place.marker(), 'click', function(){
@@ -65,56 +77,67 @@ var ViewModel = function() {
     });
   };
 
+  // A click on the marker or on the list view opens up infowindow
+  // And animates the pin
   self.clickPlace = function(place){
     infowindow.setContent(place.marker().details);
     infowindow.open(map, place.marker());
     // http://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
     place.marker().setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout(function(){ place.marker().setAnimation(null); }, 1400);
+    setTimeout(function(){ place.marker().setAnimation(null); }, 700);
     self.fourSquareDetails(place.element().url);
     /*// Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
       infowindow.place.marker() = null;
     });*/
   };
+
   // https://developer.foursquare.com/overview/tutorial
   self.fourSquareDetails = function(url){
-    $.getJSON(url, function(data) {
+    var foursquareRequestTimeout = setTimeout(function(){
+      $('#foursquare').text("Uh Oh... FourSquare is not responding!");
+    }, 4000);
+
+    $.ajax( {
+        url: url,
+        dataType: 'jsonp',
+    }).done(function( data ) {
       var venue = data.response.venue.name;
       var rating = data.response.venue.rating;
       self.fsName(venue);
       self.fsRating(rating);
+      clearTimeout(foursquareRequestTimeout);
     });
   };
 
-  //Populate the observable array with places data
+  // Populate the observable array with places data
   self.loadPlaces = function() {
     places.forEach(function(placeItem) {
       self.placesList.push( new Place(placeItem) );
     });
   };
 
-  //filtering markers by chosen category
-  //http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+  // Filtering markers by chosen category
+  // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
   self.filterCats = function(category) {
     self.catFilter(category);
-    //empty the observable array
+    // Empty the observable array
     self.filteredCats([]);
 
     var len = self.placesList().length;
-    // filter placesList array based on selected catagory
+    // Filter placesList array based on selected catagory
     var selected = ko.utils.arrayFilter(self.placesList(), function (place) {
       return place.category() == self.catFilter();
     });
-    //loop through the places list
+    // Loop through the places list
     for(var i = 0; i < len; i++) {
-      // matching category filter
+      // Matching category filter
       if (self.placesList()[i].category().indexOf(category) > -1) {
         self.filteredCats.push(self.placesList()[i]);
         self.placesList()[i].marker().setMap(map);
         map.fitBounds(bounds);
 
-      //hide other markers
+      // Hide other markers
       } else {
         self.placesList()[i].marker().setMap(null);
         map.fitBounds(bounds);
@@ -130,7 +153,7 @@ var ViewModel = function() {
     map.panTo(new google.maps.LatLng(place.marker().position.lat(), place.marker().position.lng()));
     map.setZoom(12);
     self.clickPlace(place);
-    //fourSquareDetails(place.marker().url);
+    // FourSquareDetails(place.marker().url);
   };
 
   // https://developers.google.com/maps/documentation/javascript/examples/event-domListener
@@ -184,7 +207,9 @@ var Place = function(data) {
 
   this.marker = ko.observable(marker);
   this.element = ko.observable(element);
-
+  this.path = ko.observable(this.marker.path);
+  this.fillColor = ko.observable(this.marker.fillColor);
+  this.map_icon_label = ko.observable(this.marker.map_icon_label);
 };
 
 ko.applyBindings(new ViewModel());
