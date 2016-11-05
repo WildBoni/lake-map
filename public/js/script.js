d8510e8ed4f44faa21fa4fa91653d17989d1b683
Davide Bonifacio <web@davidebonifacio.it>
2016-11-05 20:00:26 +0100

    Update: commenting and cleaning code

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// A list of great places to visit on Lake Maggiore
 var places = [
     { name: "Isola Bella", category: "panorama", id: "4bb5f9ff1344b7139acb9c04", lat: "45.89644639999999", lng: "8.526133099999999", address: "28838 Stresa VB, Italia", icon: "park"},
     { name: "Mottarone", category: "panorama", id: "4c274f81905a0f47eca56460", lat: "45.8833333", lng: "8.449999999999999", address: "28838 Stresa VB, Italia", icon: "park"},
@@ -12,21 +13,26 @@ var map;
 var infowindow = new google.maps.InfoWindow();
 var bounds = new google.maps.LatLngBounds();
 
-// FourSquare credentials
+// FourSquare API access credentials
 var clientId = 'CEE3OEEHN1QL2SS1MWKVTAH5L0MYPWFQFSGI2NZWKWMSEXIL';
 var clientSecret = '4NPFBTCE53TLCTJ5DSGEHHVKYXWTE5X5ZDIHP35SE1N0UABR';
 
-// manage everything with konckout.js
+// Let's code using knockout.js best practices
 var ViewModel = function() {
-  var venues = 'Choose a place';
+  // A useful trick for keeping track of this in scopes
   var self = this;
+
+  // Creating knockout observables for places and categories
   self.placesList = ko.observableArray([]);
   self.filteredCats = ko.observableArray([]);
   self.catFilter = ko.observable();
+
+  // Some Knockout observables for FourSquare infobox
+  var venues = 'Choose a place';
   self.fsName = ko.observable(venues);
   self.fsRating = ko.observable(venues);
 
-  // Event Listener for initializing map
+  // Event Listener for initializing map as suggested in this StackOverflow thread
   // http://stackoverflow.com/questions/36267158/uncaught-referenceerror-google-is-not-defined
   self.initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -35,8 +41,8 @@ var ViewModel = function() {
     });
   };
 
-  // Adding a listener on each marker
-  self.createMarkersClick = function(){
+  // Adding a click listener on each marker
+  self.markersClick = function(){
     self.placesList().forEach(function(place){
       google.maps.event.addListener(place.marker(), 'click', function(){
         self.clickPlace(place);
@@ -44,30 +50,25 @@ var ViewModel = function() {
     });
   };
 
-  // A click on the marker or on the list view opens up infowindow
-  // And animates the pin
+  // A click on the marker or on the list view opens up infowindow,
+  // animates the pin and updates FourSquare details box
   self.clickPlace = function(place){
-    //infowindow.setContent(place.marker().details);
-
     self.bounceMarker(place.marker());
     self.fourSquareDetails(place.element().url);
-
-
-    //From Udacity Course
-    //https://github.com/udacity/ud864/blob/master/Project_Code_6_StaticMapsAndStreetViewImagery.html#L248
+    // Getting a cool StreeView of the selected place as seen in Udacity Course
+    // https://github.com/udacity/ud864/blob/master/Project_Code_6_StaticMapsAndStreetViewImagery.html#L248
     var streetViewService = new google.maps.StreetViewService();
     var radius = 50;
     // In case the status is OK, which means the pano was found, compute the
     // position of the streetview image, then calculate the heading, then get a
     // panorama from that and set the options
     function getStreetView(data, status) {
-
       if (status == google.maps.StreetViewStatus.OK) {
         var nearStreetViewLocation = data.location.latLng;
         var heading = google.maps.geometry.spherical.computeHeading(
           nearStreetViewLocation, place.marker().position);
-          infowindow.setContent('</div><div id="pano"></div><div>' +
-          place.marker().details + '</div></div>');
+          infowindow.setContent('<div id="pano"></div><div>' +
+          place.marker().details + '</div>');
           var panoramaOptions = {
             position: nearStreetViewLocation,
             pov: {
@@ -82,32 +83,28 @@ var ViewModel = function() {
           '<div>No Street View Found</div>');
       }
     }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-
     streetViewService.getPanoramaByLocation(place.marker().position, radius, getStreetView);
     // Open the infowindow on the correct marker.
     infowindow.open(map, place.marker());
-
-
-    /*// Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.place.marker() = null;
-    });*/
   };
+
   // Marker bounces when selected
   // http://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
   self.bounceMarker = function(marker){
     marker.setAnimation(google.maps.Animation.BOUNCE);
+    // I'll make my marker bounce twice by doubling the timeout counter
     setTimeout(function(){ marker.setAnimation(null); }, 1400);
   };
 
+  // This tutorial is great for understanging how fourSquare APIs work
   // https://developer.foursquare.com/overview/tutorial
   self.fourSquareDetails = function(url){
+    // If something goes wrong, this warning will be displayed
     var foursquareRequestTimeout = setTimeout(function(){
       $('#foursquare').text("Uh Oh... FourSquare is not responding!");
-    }, 4000);
-
+    }, 6000);
+    // If the jsonp data gets retrieved correctly, we'll take infos on the
+    // venue name and rating
     $.ajax( {
         url: url,
         dataType: 'jsonp',
@@ -127,22 +124,23 @@ var ViewModel = function() {
     });
   };
 
-  // Filtering markers by chosen category
+  // The clicked category button acivates the filter
+  self.filter = function (category) {
+    self.filterCats(category);
+  };
+
+  // Filtering markers by category chosen from the buttons
   // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
   self.filterCats = function(category) {
     self.catFilter(category);
     // Empty the observable array
     self.filteredCats([]);
-
+    // Loop through the whole places list
     var len = self.placesList().length;
-    // Filter placesList array based on selected catagory
-    var selected = ko.utils.arrayFilter(self.placesList(), function (place) {
-      return place.category() == self.catFilter();
-    });
-    // Loop through the places list
     for(var i = 0; i < len; i++) {
       // Matching category filter
       if (self.placesList()[i].category().indexOf(category) > -1) {
+        // Populate the array with places from the chosen category
         self.filteredCats.push(self.placesList()[i]);
         self.placesList()[i].marker().setMap(map);
         map.fitBounds(bounds);
@@ -150,33 +148,31 @@ var ViewModel = function() {
       // Hide other markers
       } else {
         self.placesList()[i].marker().setMap(null);
-        map.fitBounds(bounds);
       }
     }
   };
 
-  self.filter = function (category) {
-    self.filterCats(category);
-  };
-
+  // Choosing a place from the list
   self.selectedPlace = function(place){
+    // Centers the map on the selected marker
     map.panTo(new google.maps.LatLng(place.marker().position.lat(), place.marker().position.lng()));
     map.setZoom(12);
     self.clickPlace(place);
-    // FourSquareDetails(place.marker().url);
   };
 
-  // https://developers.google.com/maps/documentation/javascript/examples/event-domListener
+  // Adding a Dom listener: when everything is fine, launch the app
+  // https://developers.google.com/maps/documentation/javascript/events#DomEvents
   google.maps.event.addDomListener(window, 'load', function(){
     self.initMap();
     self.loadPlaces();
     map.fitBounds(bounds);
     self.filteredCats(self.placesList());
-    self.createMarkersClick();
+    self.markersClick();
   });
 
 };
 
+// Some info about the Place
 var Place = function(data) {
   this.name = ko.observable(data.name);
   this.category = ko.observable(data.category);
@@ -186,7 +182,7 @@ var Place = function(data) {
   this.lng = ko.observable(data.lng);
   this.address = ko.observable(data.address);
   this.icon = ko.observable(data.icon);
-
+  // Transforming lat and lng strings in numbers.
   var numlat = parseFloat(this.lat());
   var numlng = parseFloat(this.lng());
   var customIcon = 'img/icons/'+ this.icon() + '.png';
@@ -198,7 +194,7 @@ var Place = function(data) {
           'Category: ' + this.category()  + '<br>' +
           this.address() + '</div>'
   });
-
+  // Place has its own FourSquare id and url
   var element = {
     id: this.id(),
     url: 'https://api.foursquare.com/v2/venues/' + this.id() + '?client_id='+ clientId +
